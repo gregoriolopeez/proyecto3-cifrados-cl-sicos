e439fd4a3ce091f08fba3b9731163c73c7551a9d
gregoriolopeez <gregoriolopeez@gmail.com>
2026-04-23 08:38:54 +0200

    subir carpetas

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    id("java")
+    id("application")
+    id("org.openjfx.javafxplugin") version "0.1.0"
 }
 
 group = "org.example"
@@ -9,11 +10,22 @@ repositories {
     mavenCentral()
 }
 
+application{
+    mainClass="hlanz.programacion.criptografia"
+}
+
+javafx{
+    modules("javafx.controls","javafx.fxml")
+    version="21"
+}
+
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
     useJUnitPlatform()
 }
+
